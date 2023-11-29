d50405079eb5be6cccb2cbfe8f00b494b16e457a
Nadja Morin <nmorin@desinata.se>
2023-11-29 16:15:01 +0100

    Initial Commit

---
+++ MP_Swift_v4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MP_Swift_v4
+//
+//  Created by Nadja on 2023-11-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MP_Swift_v4/MP_Swift_v4App.swift
@@ -0,0 +1,17 @@
+//
+//  MP_Swift_v4App.swift
+//  MP_Swift_v4
+//
+//  Created by Nadja on 2023-11-29.
+//
+
+import SwiftUI
+
+@main
+struct MP_Swift_v4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
